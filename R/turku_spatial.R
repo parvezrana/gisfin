8df0a636e9f91995b0d24e02e251feb03a015532
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-08-10 14:34:52 +0300

    Add roxygen docs for get_turku_adminboundaries
    Based on get_helsinki_aluejakokartat

--- R/turku_spatial.R
+++ R/turku_spatial.R
@@ -16,18 +16,20 @@
 #' Retrieve administrative boundaries in Turku
 #'
 #' Retrieves administrative boundaries data from Lounaispaikka (GIS repository
-#' for southwestern Finland). Available data sets and the associated licenses
-#' are:
+#' for southwestern Finland). 
+#' 
+#' Available data sets (defined by the argument \code{map.specifier}) and the 
+#' associated licenses are:
 #'
-#'\tabular{rl}{
-#'  \code{Election districts in City of Turku} \tab (C) City of turku \tab (\href{http://paikkatietokeskus.lounaispaikka.fi/cms/files/Avoin_data_Turku_lisenssiehdot.pdf}{Turun kaupungin lisenssi} \cr
+#' \describe{
+#'  \item{\code{elections}}{Election districts in City of Turku (City of turku, \href{http://paikkatietokeskus.lounaispaikka.fi/cms/files/Avoin_data_Turku_lisenssiehdot.pdf}{Turun kaupungin lisenssi})}
 #' }
 #'
 #' 
-#' @param map.specifier A string. Specify the name of the
-#' Helsinki District Boundary data set to retrieve.
-#' Run 'get_helsinki_aluejakokartat()' to see available options.
-#' @param data.dir A string. Specify a temporary folder for storing downloaded data.
+#' @param map.specifier A string. Specify the name of the Turku administrative 
+#' data set to retrieve. 
+#' @param data.dir A string. Specify a temporary folder for storing downloaded 
+#' data (default: \code{tempdir()}.
 #' @param verbose logical. Should R report extra information on progress? 
 #'
 #' @return a spatial object (from SpatialPolygonsDataFrame class)
@@ -35,12 +37,16 @@
 #' @import rgdal
 #' @import sp
 #' @export
-#' @note This function replaces sorvi::GetHRIaluejakokartat; The data is now retrieved directly from HKK.
 #'
-#' @author Juuso Parkkinen, Joona Lehtomaki and Leo Lahti \email{louhos@@googlegroups.com}
+#' @author Joona Lehtomaki, Juuso Parkkinen,and Leo Lahti \email{louhos@@googlegroups.com}
 #' @references See citation("gisfin") 
 #' @seealso \url{http://paikkatietokeskus.lounaispaikka.fi/fi/aineistot/}
 #' 
-get_turku_adminboundaries <- function(map.specifier=NULL, data.dir = tempdir(), verbose=TRUE) {
-
+#' @examples
+#' # See available options
+#' get_turku_adminboundaries() 
+#' 
+get_turku_adminboundaries <- function(map.specifier=NULL, data.dir = tempdir(), 
+                                      verbose=TRUE) {
+  return("foo")
 }
