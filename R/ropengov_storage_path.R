bcb9812c59588fba8c0aafd80299d8ce7da95077
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2017-07-09 21:11:16 +0200

    Enhance implementation
    Also return all known filepaths if no argument is provided.

--- R/ropengov_storage_path.R
+++ R/ropengov_storage_path.R
@@ -1,27 +1,39 @@
-#' @title ropengov storage path
-#' @description Storage path for rOpenGov files.
-#' @param ... Arguments to pass
-#' @return URL for Louhos data
-#' @examples url <- ropengov_storage_path()
+#' Retrieve rOpenGov storage path
+#' 
+#' Storage paths for package independent rOpenGov files.
+#' 
+#' @param key Character string key defining which storage path
+#'        is requested. If NULL (default), print available
+#'        keys and storage paths.
+#'         
+#' @return If key is provided, an URL to requested data. If not, return NULL.
+#' 
+#' @examples mml_url <- ropengov_storage_path("mml")
+#' 
 #' @export
-#' @references
-#' See citation("sorvi") 
-#' @author Leo Lahti \email{louhos@@googlegroups.com}
+#' 
+#' @author Joona Lehtomaki \email{joona.lehtomaki@@gmail.com}
 #' @keywords utilities
-ropengov_storage_path <- function(x) {
+#' 
+ropengov_storage_path <- function(key = NULL) {
   # Louhos data is stored in Github avoindata repo:
   # https://github.com/avoindata/
   storage_paths <- list(
     "mml" = "https://github.com/avoindata/mml/blob/master/rdata/",
     "louhos" = "https://github.com/avoindata/louhos/blob/master/"
   )
-  if (x %in% names(storage_paths)) {
-    storage_path <- storage_paths[x]
+  
+  # If no key is requested 
+  if (is.null(key)) {
+    print(storage_paths)
+    storage_path <- NULL
   } else {
-    stop("x is not a suitable key to stored paths: ",
-         paste(names(storage_paths), collapse = ", "))
+    if (key %in% names(storage_paths)) {
+      storage_path <- storage_paths[[key]]
+    } else {
+      stop("key is not a suitable key to stored paths: ",
+           paste(names(storage_paths), collapse = ", "))
+    }
   }
   return(storage_path)
 }
-
-
